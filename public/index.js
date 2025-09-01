c845201e4ff4bb45b572fe6fca296077ad986eca
GeoDaoyu <geodaoyu@foxmail.com>
2025-09-01 11:21:56 +0800

    chore: github pages

---
+++ public/index.js
@@ -0,0 +1,17 @@
+import "./style.css";
+import { Map, MapView, TileLayer } from "./osm-viewer.js";
+
+const map = new Map();
+const view = new MapView({
+  map,
+  container: "view",
+  center: [120, 30],
+  zoom: 4,
+});
+
+const tileLayer = new TileLayer({
+  id: "OSM Tile",
+  title: "OSM Tile",
+  url: "",
+});
+view.map.add(tileLayer);
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/osm-viewer",
   build: {
     lib: {
       entry: "./lib/main.ts",
