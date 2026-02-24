f45380ec5fe5520e777d98490ad3562b26df7db2
GeoDaoyu <geodaoyu@foxmail.com>
2026-02-24 15:29:01 +0800

    feat: implement GraphicsLayer and GraphicsLayerView

--- lib/main.ts
+++ lib/main.ts
@@ -1,5 +1,7 @@
 import Map from "./Map";
 import MapView from "./views/MapView";
 import TileLayer from "./layers/TileLayer";
+import GraphicsLayer from "./layers/GraphicsLayer";
+import GeoJSONLayer from "./layers/GeoJSONLayer";
 
-export { Map, MapView, TileLayer };
+export { Map, MapView, TileLayer, GraphicsLayer, GeoJSONLayer };
