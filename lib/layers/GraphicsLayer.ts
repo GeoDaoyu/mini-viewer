9d264fa8c84bcb01d0a459d20ec826927278568b
GeoDaoyu <geodaoyu@foxmail.com>
2026-03-01 10:36:17 +0800

    refactor: remove renderer property from GraphicsLayer

--- lib/layers/GraphicsLayer.ts
+++ lib/layers/GraphicsLayer.ts
@@ -1,22 +1,18 @@
 import Layer, { LayerProperties } from './Layer';
 import Graphic from '@/Graphic';
-import { Renderer } from '@/renderers/Renderer';
 import MapView from '@/views/MapView';
 import GraphicsLayerView from '@/views/layers/GraphicsLayerView';
 
 export interface GraphicsLayerProperties extends LayerProperties {
   graphics?: Graphic[];
-  renderer?: Renderer;
 }
 
 export default class GraphicsLayer extends Layer {
   graphics: Graphic[];
-  renderer?: Renderer;
 
   constructor(properties: GraphicsLayerProperties) {
     super(properties);
     this.graphics = properties.graphics || [];
-    this.renderer = properties.renderer;
   }
 
   createLayerView(view: MapView): GraphicsLayerView {
--- lib/views/layers/GraphicsLayerView.ts
+++ lib/views/layers/GraphicsLayerView.ts
@@ -1,10 +1,9 @@
-import LayerView from './LayerView';
-import GraphicsLayer from '../../layers/GraphicsLayer';
-import { lngLatToXY } from '../../geometry/support/webMercatorUtils';
-import { SimpleMarkerSymbol } from '../../symbols/SimpleMarkerSymbol';
-import { SimpleLineSymbol } from '../../symbols/SimpleLineSymbol';
-import { SimpleFillSymbol } from '../../symbols/SimpleFillSymbol';
-import { SimpleRenderer } from '../../renderers/SimpleRenderer';
+import LayerView from "./LayerView";
+import GraphicsLayer from "@/layers/GraphicsLayer";
+import { lngLatToXY } from "@/geometry/support/webMercatorUtils";
+import { SimpleMarkerSymbol } from "@/symbols/SimpleMarkerSymbol";
+import { SimpleLineSymbol } from "@/symbols/SimpleLineSymbol";
+import { SimpleFillSymbol } from "@/symbols/SimpleFillSymbol";
 import MapView from "../MapView";
 
 export default class GraphicsLayerView extends LayerView {
@@ -17,24 +16,50 @@ export default class GraphicsLayerView extends LayerView {
 
   async render() {
     const { canvas } = this.view;
-    const ctx = canvas.getContext('2d');
+    const ctx = canvas.getContext("2d");
     if (!ctx) return;
 
     for (const graphic of this.layer.graphics) {
-      const symbol = graphic.symbol || (this.layer.renderer instanceof SimpleRenderer ? this.layer.renderer.symbol : null);
+      const symbol = graphic.symbol;
       if (!symbol) continue;
 
-      if (graphic.geometry.type === 'point' && 'longitude' in graphic.geometry && 'latitude' in graphic.geometry) {
-        const mapPoint = lngLatToXY((graphic.geometry as any).longitude, (graphic.geometry as any).latitude);
+      if (
+        graphic.geometry.type === "point" &&
+        "longitude" in graphic.geometry &&
+        "latitude" in graphic.geometry
+      ) {
+        const mapPoint = lngLatToXY(
+          (graphic.geometry as any).longitude,
+          (graphic.geometry as any).latitude,
+        );
         const [screenX, screenY] = this.mapToScreen(mapPoint[0], mapPoint[1]);
 
-        if (symbol.type === 'simple-marker') {
-          this.renderMarker(ctx, screenX, screenY, symbol as SimpleMarkerSymbol);
+        if (symbol.type === "simple-marker") {
+          this.renderMarker(
+            ctx,
+            screenX,
+            screenY,
+            symbol as SimpleMarkerSymbol,
+          );
         }
-      } else if (graphic.geometry.type === 'polyline' && 'paths' in graphic.geometry) {
-        this.renderPolyline(ctx, (graphic.geometry as any).paths, symbol as SimpleLineSymbol);
-      } else if (graphic.geometry.type === 'polygon' && 'rings' in graphic.geometry) {
-        this.renderPolygon(ctx, (graphic.geometry as any).rings, symbol as SimpleFillSymbol);
+      } else if (
+        graphic.geometry.type === "polyline" &&
+        "paths" in graphic.geometry
+      ) {
+        this.renderPolyline(
+          ctx,
+          (graphic.geometry as any).paths,
+          symbol as SimpleLineSymbol,
+        );
+      } else if (
+        graphic.geometry.type === "polygon" &&
+        "rings" in graphic.geometry
+      ) {
+        this.renderPolygon(
+          ctx,
+          (graphic.geometry as any).rings,
+          symbol as SimpleFillSymbol,
+        );
       }
     }
   }
@@ -64,80 +89,97 @@ export default class GraphicsLayerView extends LayerView {
     return [screenX, screenY];
   }
 
-  private renderMarker(ctx: CanvasRenderingContext2D, x: number, y: number, symbol: SimpleMarkerSymbol) {
+  private renderMarker(
+    ctx: CanvasRenderingContext2D,
+    x: number,
+    y: number,
+    symbol: SimpleMarkerSymbol,
+  ) {
     const size = parseInt(symbol.size) || 8;
     const radius = size / 2;
 
     ctx.fillStyle = `rgba(${symbol.color.r}, ${symbol.color.g}, ${symbol.color.b}, ${symbol.color.a})`;
 
-    if (symbol.style === 'circle') {
+    if (symbol.style === "circle") {
       ctx.beginPath();
       ctx.arc(x, y, radius, 0, 2 * Math.PI);
       ctx.fill();
-    } else if (symbol.style === 'square') {
+    } else if (symbol.style === "square") {
       ctx.fillRect(x - radius, y - radius, size, size);
     }
 
     if (symbol.outline) {
       ctx.strokeStyle = `rgba(${symbol.outline.color.r}, ${symbol.outline.color.g}, ${symbol.outline.color.b}, ${symbol.outline.color.a})`;
-      ctx.lineWidth = 1; 
+      ctx.lineWidth = 1;
 
-      if (symbol.style === 'circle') {
+      if (symbol.style === "circle") {
         ctx.beginPath();
         ctx.arc(x, y, radius, 0, 2 * Math.PI);
         ctx.stroke();
-      } else if (symbol.style === 'square') {
+      } else if (symbol.style === "square") {
         ctx.strokeRect(x - radius, y - radius, size, size);
       }
     }
   }
 
-  private renderPolyline(ctx: CanvasRenderingContext2D, paths: number[][][], symbol: SimpleLineSymbol) {
+  private renderPolyline(
+    ctx: CanvasRenderingContext2D,
+    paths: number[][][],
+    symbol: SimpleLineSymbol,
+  ) {
     ctx.beginPath();
-    
+
     for (const path of paths) {
       if (path.length === 0) continue;
-      
+
       const [firstLng, firstLat] = path[0];
-      const [firstX, firstY] = this.mapToScreen(...lngLatToXY(firstLng, firstLat));
+      const [firstX, firstY] = this.mapToScreen(
+        ...lngLatToXY(firstLng, firstLat),
+      );
       ctx.moveTo(firstX, firstY);
-      
+
       for (let i = 1; i < path.length; i++) {
         const [lng, lat] = path[i];
         const [x, y] = this.mapToScreen(...lngLatToXY(lng, lat));
         ctx.lineTo(x, y);
       }
     }
-    
+
     ctx.strokeStyle = `rgba(${symbol.color.r}, ${symbol.color.g}, ${symbol.color.b}, ${symbol.color.a})`;
     ctx.lineWidth = symbol.width || 1;
     ctx.stroke();
   }
 
-  private renderPolygon(ctx: CanvasRenderingContext2D, rings: number[][][], symbol: SimpleFillSymbol) {
+  private renderPolygon(
+    ctx: CanvasRenderingContext2D,
+    rings: number[][][],
+    symbol: SimpleFillSymbol,
+  ) {
     ctx.beginPath();
-    
+
     for (const ring of rings) {
       if (ring.length === 0) continue;
-      
+
       const [firstLng, firstLat] = ring[0];
-      const [firstX, firstY] = this.mapToScreen(...lngLatToXY(firstLng, firstLat));
+      const [firstX, firstY] = this.mapToScreen(
+        ...lngLatToXY(firstLng, firstLat),
+      );
       ctx.moveTo(firstX, firstY);
-      
+
       for (let i = 1; i < ring.length; i++) {
         const [lng, lat] = ring[i];
         const [x, y] = this.mapToScreen(...lngLatToXY(lng, lat));
         ctx.lineTo(x, y);
       }
-      
+
       ctx.closePath();
     }
-    
+
     if (symbol.color) {
       ctx.fillStyle = `rgba(${symbol.color.r}, ${symbol.color.g}, ${symbol.color.b}, ${symbol.color.a})`;
       ctx.fill();
     }
-    
+
     if (symbol.outline) {
       ctx.strokeStyle = `rgba(${symbol.outline.color.r}, ${symbol.outline.color.g}, ${symbol.outline.color.b}, ${symbol.outline.color.a})`;
       ctx.lineWidth = symbol.outline.width || 1;
