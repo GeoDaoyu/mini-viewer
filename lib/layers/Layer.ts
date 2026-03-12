3e4ae00057a3a2a821d5faa2d4d7b93ed717f2e2
GeoDaoyu <geodaoyu@foxmail.com>
2026-03-12 16:34:56 +0800

    refactor: extract LayerType to support file

--- lib/layers/Layer.ts
+++ lib/layers/Layer.ts
@@ -1,57 +1,6 @@
 import MapView from "@/views/MapView";
 import LayerView from "@/views/layers/LayerView";
-
-export type LayerType =
-  | "base-dynamic"
-  | "base-elevation"
-  | "base-tile"
-  | "bing-maps"
-  | "building-scene"
-  | "catalog"
-  | "catalog-footprint"
-  | "catalog-dynamic-group"
-  | "csv"
-  | "dimension"
-  | "elevation"
-  | "feature"
-  | "geo-rss"
-  | "geojson"
-  | "parquet"
-  | "gaussian-splat"
-  | "graphics"
-  | "group"
-  | "imagery"
-  | "imagery-tile"
-  | "integrated-mesh"
-  | "integrated-mesh-3dtiles"
-  | "kml"
-  | "knowledge-graph"
-  | "knowledge-graph-sublayer"
-  | "line-of-sight"
-  | "link-chart"
-  | "map-image"
-  | "map-notes"
-  | "media"
-  | "video"
-  | "ogc-feature"
-  | "open-street-map"
-  | "oriented-imagery"
-  | "point-cloud"
-  | "route"
-  | "scene"
-  | "stream"
-  | "subtype-group"
-  | "tile"
-  | "unknown"
-  | "unsupported"
-  | "vector-tile"
-  | "viewshed"
-  | "voxel"
-  | "wcs"
-  | "web-tile"
-  | "wfs"
-  | "wms"
-  | "wmts";
+import type { LayerType } from "./types";
 
 export interface LayerProperties {
   id: string;
@@ -73,3 +22,5 @@ export default class Layer {
     return new LayerView({ view, layer: this });
   }
 }
+
+export type { LayerType };
